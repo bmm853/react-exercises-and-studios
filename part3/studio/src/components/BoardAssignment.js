d98bb8c60402605e0505730353dda56b68f25c50
bmm853 <rebeccawnelson@gmail.com>
2023-08-07 17:09:18 -0500

    Completed studio requirements

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [ 
+      {label: "Salads", value: "Salads"},
+      {label: "Cabbage Recipes", value: "Cabbage Recipes"},
+      {label: "Asian-Inspired Flavors", value: "Asian-Inspired Flavors"}
+   ];
+
+   const [boardName, setName] = useState("no boards yet!");
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+            <option value={board.value}>{board.label}</option>
+         ))};
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,21 +1,21 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.budgetbytes.com/about/";
+   let authorPhoto = "https://www.budgetbytes.com/wp-content/uploads/2022/07/Beth-2022-3-SQ-380x380.jpg";
+   let authorName = "Beth Moncel";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Beth Moncel 2022" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>About Beth</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["shredded purple cabbage", "carrots", "green onion", "tahini", "grated fresh ginger"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Crunchy Cabbage Salad</h1>
+            <p>crunchy red cabbage and carrots coated in a yummy sesame ginger dressing</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +47,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.budgetbytes.com/wp-content/uploads/2012/02/Crunchy-Cabbage-Salad-dressing-V2.jpg" alt="Crunchy Cabbage Salad in a white serving dish with two forks, drizzled with sesame ginger dressing" className="imageUpdates"/>
    );
 }
 
